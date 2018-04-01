711b22a4c204ec5fdd771995277ebf441959b377
Whdigger <landstage@yandex.ru>
2018-04-01 23:49:45 +0300

    02_lesson Вырождающиеся объекты ~ Устранения побочных эффектов в классе Dollar

--- src/Finance/Dollar.php
+++ src/Finance/Dollar.php
@@ -1,7 +1,18 @@
 <?php
+
 namespace Finance;
 
 class Dollar
 {
+    public $amount;
     
+    public function __construct(int $amount)
+    {
+        $this->amount = $amount;
+    }
+    
+    public function times(int $multiplier)
+    {
+        return new $this($this->amount * $multiplier);
+    }
 }
--- tests/Finance/DollarTest.php
+++ tests/Finance/DollarTest.php
@@ -1,9 +1,16 @@
 <?php
-namespace Finance;
 
+use Finance\Dollar;
 use PHPUnit\Framework\TestCase;
 
 class DollarTest extends TestCase
 {
-    
+    public function testMultiplication(): void
+    {
+        $five = new Dollar(5);
+        $product = $five->times(2);
+        $this->assertEquals(10, $product->amount);
+        $product = $five->times(3);
+        $this->assertEquals(15, $product->amount);
+    }
 }
