bb62d6e42299f8bb204000d485e29b7c01a44954
Whdigger <landstage@yandex.ru>
2018-04-07 23:32:21 +0300

    08_lesson Создание объектов + Общая сигнатура для методов times, фабричные методы

--- src/Finance/Money.php
+++ src/Finance/Money.php
@@ -2,7 +2,7 @@
 
 namespace Finance;
 
-class Money
+abstract class Money
 {
     protected $amount;
     
@@ -12,4 +12,27 @@ class Money
             $this->amount == $money->amount &&
             get_class($this) == get_class($money);
     }
+    
+    abstract public function times(int $multiplier);
+    
+    /**
+     * @param int $amount
+     *
+     * @return Dollar
+     */
+    public static function dollar(int $amount)
+    {
+        return new Dollar($amount);
+    }
+    
+    /**
+     * @param int $amount
+     *
+     * @return Franc
+     */
+    public static function franc(int $amount)
+    {
+        return new Franc($amount);
+    }
+    
 }
