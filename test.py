a91ee41b08f211a19efd1587fb1dbcf572335382
Whdigger <landstage@yandex.ru>
2019-03-24 15:31:03 +0300

    18_lesson Первыер шаги xUnit

---
+++ test.py
@@ -0,0 +1,14 @@
+class WasRun:
+  def __init__(self,name):
+    self.wasRun = None
+    
+def testMethod(self):
+  self.wasRun = 1
+
+  def testMethod(self):
+    self.wasRun = 1
+    
+test = WasRun("testMethod")
+print(test.wasRun)
+test.run()
+print(test.wasRun)
